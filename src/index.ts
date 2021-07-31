c9bbf2f61be74a3dc8d67a12816849f144890316
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-07-31 18:31:39 +1200

    Add Socket.IO

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import http from 'http'
 import { config } from './config/config'
 import { app } from './app'
+import { Server, Socket } from 'socket.io'
 
 const port = config.PORT
 
 const httpServer = new http.Server(app)
+
+const io = new Server(httpServer, {
+  path: '/socket.io',
+})
+
+io.on('connection', (socket: Socket) => {
+  console.log('user connected', socket)
+})
 
 const server = httpServer.listen(port, function () {
   console.log(`Listening on *:${port}`)
