7448f751aab18ad2abc593c0f8a44d5f8a66d9a7
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-07-31 22:58:42 +1200

    Update config to read from base64

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,12 +2,35 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const firebaseServiceAccountIsPath = process.env.FIREBASE_SERVICE_ACCOUNT_IS_PATH
+
+let FIREBASE_SERVICE_ACCOUNT = './config/serviceAccount.json'
+
+const encodeToBase64 = (data: string): string => {
+  const base64 = Buffer.from(data).toString('base64')
+  return base64
+}
+
+const decodeFromBase64 = (base64: string): string => {
+  const text = Buffer.from(base64, 'base64').toString('ascii')
+  return text
+}
+
+if (firebaseServiceAccountIsPath === 'true') {
+  FIREBASE_SERVICE_ACCOUNT = process.env.FIREBASE_SERVICE_ACCOUNT
+} else {
+  // It is JSON stringified
+  const decoded = decodeFromBase64(process.env.FIREBASE_SERVICE_ACCOUNT)
+  FIREBASE_SERVICE_ACCOUNT = JSON.parse(decoded)
+}
+
 const config = {
   PORT: process.env.PORT || 4000,
   MONGODB_URI: process.env.MONGODB_URI,
-  FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT || './config/serviceAccount.json',
+  FIREBASE_SERVICE_ACCOUNT: FIREBASE_SERVICE_ACCOUNT,
   GITHUB_VSCODE_CLIENT_ID: process.env.GITHUB_VSCODE_CLIENT_ID,
   GITHUB_VSCODE_CLIENT_SECRET: process.env.GITHUB_VSCODE_CLIENT_SECRET,
+  FIREBASE_SERVICE_ACCOUNT_IS_PATH: process.env.FIREBASE_SERVICE_ACCOUNT_IS_PATH,
 }
 
 export { config }
